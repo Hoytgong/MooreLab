8a7ef4eae899c2c3250c189c318960689a2eb404
Hoyt <hoytgong@wharton.upenn.edu>
2018-11-03 12:01:02 -0400

    reran tpot

--- bcdata_pipeline.py
+++ bcdata_pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from xgboost import XGBClassifier
+from sklearn.svm import LinearSVC
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +9,8 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=None)
 
-# Average CV score on the training set was:0.5654218382536744
-exported_pipeline = XGBClassifier(learning_rate=0.001, max_depth=2, min_child_weight=13, n_estimators=100, nthread=1, subsample=0.8)
+# Average CV score on the training set was:0.5698434622467772
+exported_pipeline = LinearSVC(C=20.0, dual=True, loss="hinge", penalty="l2", tol=0.0001)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
