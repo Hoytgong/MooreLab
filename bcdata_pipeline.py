42bce79c29b26c68ed5cd3168ee824aeb03c95de
Hoyt <hoytgong@wharton.upenn.edu>
2019-06-21 15:52:25 -0400

    big update

--- bcdata_pipeline.py
+++ bcdata_pipeline.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +11,11 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=None)
 
-# Average CV score on the training set was:0.5596198830409357
-exported_pipeline = ExtraTreesClassifier(bootstrap=False, criterion="entropy", max_features=0.1, min_samples_leaf=18, min_samples_split=20, n_estimators=100)
+# Average CV score on the training set was:0.560727916281933
+exported_pipeline = make_pipeline(
+    PCA(iterated_power=1, svd_solver="randomized"),
+    LogisticRegression(C=0.001, dual=False, penalty="l2")
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
